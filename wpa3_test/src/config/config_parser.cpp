bedbbbd7aa986a95a1ce8850c916abe370819634
Vit Janecek <xjanecv00@vutbr.cz>
2025-12-20 12:21:55 +0100

    f: attack config

--- wpa3_test/src/config/config_parser.cpp
+++ wpa3_test/src/config/config_parser.cpp
@@ -38,14 +38,36 @@ json yaml_to_json(const YNode& node) {
 void RunStatus::config_validation() {
     try {
         YNode config_node = YAML::LoadFile(this->finalPath);
-        fs::path schema_path = fs::path(PROJECT_ROOT_DIR) / "attack_config" / "validator" / "test_validator.yaml";
-        YAML::Node schema_node = YAML::LoadFile(schema_path.string());
+        json config_json = yaml_to_json(config_node);
 
-        nlohmann::json_schema::json_validator validator;
-        validator.set_root_schema(yaml_to_json(schema_node));
+        fs::path global_schema_path = fs::path(PROJECT_ROOT_DIR) \
+            / "attack_config" / "validator" / "test_validator.yaml";
+        YNode global_schema_node = YAML::LoadFile(global_schema_path.string());
 
-        nlohmann::json_schema::basic_error_handler err;
-        validator.validate(yaml_to_json(config_node), err);
+        nlohmann::json_schema::json_validator global_validator;
+        global_validator.set_root_schema(yaml_to_json(global_schema_node));
+        global_validator.validate(config_json);
+
+        if (!config_json.contains("attack_config") || !config_json["attack_config"].is_object()) {
+            throw config_error("Missing or invalid 'attack_config' section in config");
+        }
+
+        json attack_cfg = config_json["attack_config"];
+        if (!attack_cfg.contains("validator") || !attack_cfg["validator"].is_string()) {
+            throw config_error("'attack_config.validator' must be a string path to YAML schema");
+        }
+
+        fs::path config_path(this->finalPath);
+        fs::path config_dir = config_path.parent_path();
+        fs::path attack_schema_path = config_dir \
+            / attack_cfg["validator"].get<string>();
+
+        YNode attack_schema_node = YAML::LoadFile(attack_schema_path.string());
+        json attack_schema_json = yaml_to_json(attack_schema_node);
+
+        nlohmann::json_schema::json_validator attack_validator;
+        attack_validator.set_root_schema(attack_schema_json);
+        attack_validator.validate(attack_cfg);
 
     } catch (const domain_error &e) {
         throw config_error::format("Schema error: {}", e.what());

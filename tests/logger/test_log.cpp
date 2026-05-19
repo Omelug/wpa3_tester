8252edf1e46a206e3485cac64f511da28a124497
Omelug <@>
2026-05-19 17:59:42 +0200

    t: log.cpp simple parse tests

--- tests/logger/test_log.cpp
+++ tests/logger/test_log.cpp
@@ -1,12 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest.h>
 #include <ctime>
-#include <cstdint>
-#include <chrono>
+#include <doctest.h>
 #include <filesystem>
 #include <fstream>
 #include <string>
 
+#include "config/ActorPtr.h"
+#include "config/actor_keys.h"
 #include "config/RunStatus.h"
 #include "logger/log.h"
 
@@ -27,4 +27,41 @@ TEST_CASE("log - debug message with actor name"){
     cerr.rdbuf(original_cerr);
 
     CHECK_NE(captured_output.str().find(expected_message), string::npos);
+}
+
+TEST_CASE("log - set_log_file writes messages to file"){
+    const filesystem::path tmp = "/tmp/wpa3_test_log.txt";
+    filesystem::remove(tmp);
+
+    wpa3_tester::set_log_file(tmp);
+    wpa3_tester::log(wpa3_tester::LogLevel::INFO, "file_log_test");
+    wpa3_tester::set_log_file("");  // close
+
+    ifstream f(tmp);
+    const string content((istreambuf_iterator(f)), istreambuf_iterator<char>());
+    filesystem::remove(tmp);
+
+    CHECK_NE(content.find("file_log_test"), string::npos);
+}
+
+TEST_CASE("log - log_actor_map prints name and keys"){
+    wpa3_tester::ActorCMap m;
+    m["alpha"] = wpa3_tester::ActorPtr{};
+    m["beta"]  = wpa3_tester::ActorPtr{};
+
+    ostringstream captured;
+    streambuf* orig = cerr.rdbuf(captured.rdbuf());
+    wpa3_tester::log_actor_map("my_map", m);
+    cerr.rdbuf(orig);
+
+    const string out = captured.str();
+    CHECK_NE(out.find("my_map:"), string::npos);
+    CHECK_NE(out.find("alpha"), string::npos);
+    CHECK_NE(out.find("beta"), string::npos);
+}
+
+TEST_CASE("log - escape_tex replaces underscores"){
+    CHECK_EQ(wpa3_tester::escape_tex("hello_world"),   "hello\\_world");
+    CHECK_EQ(wpa3_tester::escape_tex("a_b_c"),         "a\\_b\\_c");
+    CHECK_EQ(wpa3_tester::escape_tex("no underscores"), "no underscores");
 }

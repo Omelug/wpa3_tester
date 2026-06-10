e13c24dfef8b6ff92aafe51a887ce71c7245ca89
Vit Janecek <xjanecv00@vutbr.cz>
2026-06-10 10:38:20 +0200

    f: modprobe delete before test

--- wpa3_test/src/setup/requirement_validation.cpp
+++ wpa3_test/src/setup/requirement_validation.cpp
@@ -227,6 +227,7 @@ ActorCMap get_actors(const ActorCMap &actors, const string &source){
 }
 
 bool RunStatus::config_requirement(){
+	hw_capabilities::run_cmd({"modprobe", "-r", "mac80211_hwsim"}, nullopt, false);
 	check_local_requirements();
 	firmware::disable_custom_drivers();
 	cleanup_all_namespaces();

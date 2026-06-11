6cd2e73de1655318df0f406137e915e456c78bec
Vit Janecek <xjanecv00@vutbr.cz>
2026-06-11 21:37:38 +0200

    +: sta_info base 2

---
+++ wpa3_test/include/scan/active/scan_active.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <tins/pdu.h>
+#include <tins/dot11.h>
+#include "config/Actor_Config/Actor_Config_external.h"
+
+namespace wpa3_tester::scan{
+void apply_radiotap(Tins::PDU &pdu, Actor_Config_external &cfg);
+void apply_ht_vht_he(const Tins::Dot11ManagementFrame &mgmt, Actor_Config_external &cfg);
+
+}

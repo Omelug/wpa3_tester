fcf1ab9605c7e445483f0424d577abeee6f8e410
Vit Janecek <xjanecv00@vutbr.cz>
2026-08-09 15:14:12 +0200

    +: defalt.h path

--- wpa3_test/include/default.h
+++ wpa3_test/include/default.h
@@ -2,12 +2,18 @@
 
 inline char CSV_SEP = '|';
 
+inline std::string TESTER_NAME = "wpa3_tester";
+inline std::string DATA_TEST = "test_data";
+inline std::string DATA_SUITE = "suite_data";
+
+// data / output
 inline std::string REPORT_NAME = "report.md";
 inline std::string INDEX_HTML = "index.html";
 inline std::string RESULT_NAME = "result.json";
 inline std::string TEST_CONFIG_NAME = "test_config.yaml";
 inline std::string TEST_SUITE_CONFIG_DIR = "test_config";
 
+// in taest folder
 inline std::string ERROR_FILE = "errors.txt";
 inline std::string DONE_FILE = "done.txt";
 inline std::string MAPPING_CSV = "mapping.csv";

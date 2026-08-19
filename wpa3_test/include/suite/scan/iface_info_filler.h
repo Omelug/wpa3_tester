5bb32433e394074991b80f9f72da70ee06035086
TorShell <torshell@fakeemail.edu>
2026-08-19 20:10:23 +0200

    +: driver diagnostics

--- wpa3_test/include/suite/scan/iface_info_filler.h
+++ wpa3_test/include/suite/scan/iface_info_filler.h
@@ -8,11 +8,10 @@ namespace wpa3_tester::suite::iface_info_filler{
 struct IfaceInfoTestEntry{
 	std::string test_name;
 	std::string hw_summary;
+	std::string driver_summary;
 	std::filesystem::path report_md;
 
 	static IfaceInfoTestEntry parse(const std::filesystem::path &test_folder);
-	std::vector<IfaceInfoTestEntry>
-	collect_results(const std::filesystem::path &test_data_dir) const;
 };
 
 std::vector<IfaceInfoTestEntry> collect_results(const std::filesystem::path &run_dir);

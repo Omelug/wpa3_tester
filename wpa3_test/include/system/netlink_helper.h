31424e8866080bc672246487d924782f2b1963ea
TorShell <torshell@fakeemail.edu>
2026-08-20 19:25:35 +0200

    +: iface_info - channel_switch, netns_move ch: delete_ns_and_wait - only move to netlink_hleper +: delete WifiVirtualMon/WifiVirtualAP before test in config_requirement

--- wpa3_test/include/system/netlink_helper.h
+++ wpa3_test/include/system/netlink_helper.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <chrono>
+#include <string>
 #include <string_view>
 #include <system_error>
+#include <vector>
 #include <linux/nl80211.h>
 #include "system/wifi_channel.h"
 
@@ -22,8 +25,11 @@ nl80211_iftype query_wifi_iftype(std::string_view iface_name, const std::optiona
 [[nodiscard]] Result wait_for_wifi_iftype(std::string_view iface_name, const std::optional<std::string> &netns,
 										nl80211_iftype expected_type, int max_retries = 50, int retry_ms = 100
 );
-[[nodiscard]] Result wait_for_channel(std::string_view iface_name, const std::optional<std::string> &netns,
+Result wait_for_channel(std::string_view iface_name, const std::optional<std::string> &netns,
 									const Channel &ch, int max_retries = 50, int retry_ms = 100
 );
 [[nodiscard]] Result set_channel_nl(std::string_view iface, const std::optional<std::string> &netns, const Channel &ch);
+
+void delete_ns_and_wait(const std::string &ns_name, const std::vector<std::string> &ifaces,
+                        std::chrono::milliseconds timeout = std::chrono::milliseconds(3000));
 }

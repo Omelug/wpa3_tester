ca1e4f311e9631841940c94497c1ef22e6234260
TorShell <torshell@fakeemail.edu>
2026-08-28 20:03:25 +0200

    -: save log options in run config (save test.log everytime )

--- wpa3_test/src/config/RunStatus.cpp
+++ wpa3_test/src/config/RunStatus.cpp
@@ -82,11 +82,10 @@ void RunStatus::execute(){
 
 	create_public_dirs(_run_folder);
 
-	// Initialize log file if save_log is enabled
-	if(_run_config.get_save_log()){
-		const path log_file = _run_folder / "logger" / "tester.log";
-		set_log_file(log_file);
-	}
+	// Initialize log file
+	const path log_file = _run_folder / "logger" / "tester.log";
+	set_log_file(log_file);
+
 	struct LogGuard{
 		~LogGuard(){ close_log_file(); }
 	} log_guard;

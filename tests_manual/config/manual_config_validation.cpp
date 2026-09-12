002fa65ce59056100495e56703e7cea01132c7ee
TorShell <torshell@fakeemail.edu>
2026-09-12 17:11:20 +0200

    t: test validation - testovat i vygenerované testy?

--- tests_manual/config/manual_config_validation.cpp
+++ tests_manual/config/manual_config_validation.cpp
@@ -26,8 +26,18 @@ int main(){
         const string rel = relative(entry.path(), ATTACK_CONFIG()).string();
         try{
             if(is_suite(entry.path())){
-	            RunSuiteStatus::config_validation(entry.path());
-
+	            const auto suite_json = RunSuiteStatus::config_validation(entry.path());
+	            for(const auto &[src_name, src_info] : suite_json.at("tests").items()){
+	                if(src_info.value("type", "") == "actor_filler"){
+	                    cout << "[SKIP] " << rel << "/" << src_name << " (actor_filler)\n";
+	                    continue;
+	                }
+	                if(src_info.contains("path")){
+	                    RunStatus::config_validation(absolute(entry.path().parent_path() / src_info.at("path").get<string>()));
+	                } else if(src_info.contains("test_name")){
+	                    RunStatus::config_validation(RunStatus::findConfigByTestName(src_info.at("test_name").get<string>()));
+	                }
+	            }
             }else{
 	            RunStatus::config_validation(entry.path());
             }
